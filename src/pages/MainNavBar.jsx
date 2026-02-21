05c4e38ced074506c7c59fcf3262c3cf6a42efaa
yojitha <2500031071@kluniversity.in>
2026-02-21 10:19:17 +0530

    modified MainNavBar.jsx

--- src/pages/MainNavBar.jsx
+++ src/pages/MainNavBar.jsx
@@ -15,7 +15,7 @@ const MainNavBar = () => {
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
-            Event Management System
+           KL Event Management System
           </Link>
         </div>
         <ul className="nav-menu">
